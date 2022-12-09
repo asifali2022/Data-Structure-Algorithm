0ce54b1f16b719bbff59248eb46ceff98372260e
asifali2022 <asifalisma247@gmail.com>
2022-12-09 19:52:49 +0530

    first commit

---
+++ dataStructureAlgorithm/src/mathematics/TrailingZeroesInFactorials.java
@@ -0,0 +1,30 @@
+package mathematics;
+
+class trailing{
+	int a;
+	public int zeroCounter(int a)
+	{
+		int sum=1;
+		while(a>0)
+		{
+			sum=sum*a;
+			a--;
+		}
+		int count=0;
+		while(sum%10==0) 
+		{    sum=sum/10;
+			count++;
+		}
+		return count;
+	}
+}
+
+public class TrailingZeroesInFactorials {
+
+	public static void main(String[] args) {
+		
+		trailing v=new trailing();
+		 System.out.println(v.zeroCounter(12));
+	}
+
+}
---
+++ dataStructureAlgorithm/src/mathematics/pallindromeNumber.java
@@ -0,0 +1,24 @@
+
+package mathematics;
+
+
+class checkPallindrome{
+	
+	public int checkpallindromeNumber(int a) 
+	{
+		while(a>0)
+		{
+			
+		
+		}
+		return a;	
+	}
+}
+public class pallindromeNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
